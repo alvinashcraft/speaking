83fdd8dda3ca6f4e2e25373a72e2bb6f7ad91d1a
Alvin Ashcraft <alashcraft@gmail.com>
2024-04-06 17:48:20 -0400

    Add VSLive Chicago 2024 materials

---
+++ 2024/0430_VSLiveChicago/ContributeToMsLearn/links.md
@@ -0,0 +1,32 @@
+# Links from VSLive Chicago 2024
+
+## Learn, Share, and Grow: How to Become a Microsoft Learn Contributor
+
+### Alvin Ashcraft's links
+
+- [Alvin on LinkedIn](https://www.linkedin.com/in/alvinashcraft/)
+- [Alvin's blog](https://www.alvinashcraft.com/)
+- [Alvin on Twitter](https://twitter.com/alvinashcraft)
+- [Alvin's GitHub](https://github.com/alvinashcraft)
+- [Buy my books](https://www.amazon.com/stores/Alvin-Ashcraft/author/B08WLD35BX)
+
+### Microsoft Learn links
+
+- [Documentation](https://learn.microsoft.com/docs/)
+- [Training](https://learn.microsoft.com/training/)
+- [Q&A](https://learn.microsoft.com/answers/)
+- [Contributor Home](https://learn.microsoft.com/contribute/)
+- [Contributor Guide](https://learn.microsoft.com/Contribute/content/)
+- [Learn Authoring Pack for VS Code](https://learn.microsoft.com/contribute/how-to-write-docs-auth-pack)
+- [Microsoft Writing Style Guide](https://learn.microsoft.com/style-guide/welcome/)
+- [MicrosoftDocs GitHub Org](https://github.com/microsoftdocs)
+
+### Other Links
+
+- [TechBash Developer Conference](https://techbash.com/)
+- [Azure Product Feedback](https://feedback.azure.com/)
+- [.NET Product Support](https://dotnet.microsoft.com/platform/support)
+- [GitHub Workflow for a new post](https://github.com/PowerShell/Community-Blog/blob/main/Docs/GitHub-workflow-for-new-post.md)
+- [GitHub Learning Paths](https://learn.microsoft.com/training/github)
+- [Git training](https://learn.microsoft.com/training/paths/intro-to-vc-git)
+- [Git in VS Code training](https://learn.microsoft.com/training/modules/use-git-from-vs-code)
---
+++ 2024/0430_VSLiveChicago/WindowsDev/ShoppingList.Uno/UnoMediaCollection/Shared/ShoppingListHelpers.cs
@@ -0,0 +1,37 @@
+﻿namespace ShoppingListSample.Shared
+{
+    public static class ShoppingListHelpers
+    {
+        public static IList<Category> CreateCategories()
+        {
+            return new List<Category>
+            {
+                new() { Name = "Produce", CategoryId = 1 },
+                new() { Name = "Dairy", CategoryId = 2 },
+                new() { Name = "Bakery", CategoryId = 3 },
+                new() { Name = "Meat", CategoryId = 4 },
+                new() { Name = "Frozen", CategoryId = 5 },
+                new() { Name = "Canned", CategoryId = 6 },
+                new() { Name = "Beverages", CategoryId = 7 },
+                new() { Name = "Snacks", CategoryId = 8 }
+            };
+        }
+
+        public static IList<Item> CreateDemoShoppingListItems()
+        {
+            return new List<Item>
+            {
+                new() { Name = "Apples", Category = new Category() { Name = "Produce" } },
+                new() { Name = "Bananas", Category = new Category() { Name = "Produce" } },
+                new() { Name = "Oranges", Category = new Category() { Name = "Produce" }, IsComplete = true },
+                new() { Name = "Milk", Category = new Category() { Name = "Dairy" } },
+                new() { Name = "Eggs", Category = new Category() { Name = "Dairy" }, IsComplete = true },
+                new() { Name = "Bread", Category = new Category() { Name = "Bakery" }, IsComplete = true },
+                new() { Name = "Chicken", Category = new Category() { Name = "Meat" } },
+                new() { Name = "Beef", Category = new Category() { Name = "Meat" } },
+                new() { Name = "Pork", Category = new Category() { Name = "Meat" }, IsComplete = true },
+                new() { Name = "Ice Cream", Category = new Category() { Name = "Frozen" } }
+            };
+        }
+    }
+}
