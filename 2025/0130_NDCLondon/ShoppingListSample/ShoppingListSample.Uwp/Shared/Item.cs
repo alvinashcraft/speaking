9e5c455e5002b401eaf5e4fde1244a28cc433652
Alvin Ashcraft <73072+alvinashcraft@users.noreply.github.com>
2024-12-31 13:38:20 -0500

    Add windev code for NDC London 25

---
+++ 2025/0130_NDCLondon/ShoppingListSample/ShoppingListSample.Uwp/Shared/Item.cs
@@ -0,0 +1,18 @@
+﻿namespace ShoppingListSample.Shared
+{
+    public class Item
+    {
+        public Item()
+        {
+            Name = string.Empty;
+            Category = new Category();
+        }
+        public string Name { get; set; }
+        public virtual Category Category { get; set; }
+        public bool IsComplete { get; set; }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
