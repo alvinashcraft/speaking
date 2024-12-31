6a8df3e65c313627339fc33dd8d80563f7c71692
Alvin Ashcraft <73072+alvinashcraft@users.noreply.github.com>
2024-12-31 13:42:57 -0500

    Add WinUI base materials for VSLive Vegas 25

---
+++ 2025/0311_VSLiveLasVegas/Getting Started with WinUI/UnoMediaCollection/UnoMediaCollection/ItemDetailsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using UnoMediaCollection.ViewModels;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace UnoMediaCollection
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ItemDetailsPage : Page
+    {
+        public ItemDetailsPage()
+        {
+            ViewModel = App.HostContainer.Services.GetService<ItemDetailsViewModel>();
+            this.InitializeComponent();
+        }
+
+        public ItemDetailsViewModel ViewModel;
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            var itemId = (int)e.Parameter;
+
+            if (itemId > 0)
+            {
+                ViewModel.InitializeItemDetailData(itemId);
+            }
+        }
+    }
+}
