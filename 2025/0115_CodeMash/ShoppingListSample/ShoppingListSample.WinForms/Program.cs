6357502b66aac0f564aef92b23536be6423b6997
Alvin Ashcraft <73072+alvinashcraft@users.noreply.github.com>
2025-01-16 11:00:17 -0500

    Last minute codemash tweaks
    Update slides and add dark mode code to winforms sample.

--- 2025/0115_CodeMash/ShoppingListSample/ShoppingListSample.WinForms/Program.cs
+++ 2025/0115_CodeMash/ShoppingListSample/ShoppingListSample.WinForms/Program.cs
@@ -11,6 +11,9 @@ namespace ShoppingListSample.WinForms
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+//#pragma warning disable WFO5001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+//            Application.SetColorMode(SystemColorMode.Dark);
+//#pragma warning restore WFO5001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
             Application.Run(new ShoppingListForm());
         }
     }
