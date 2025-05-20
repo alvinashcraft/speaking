8e6e979cdbb3f71dc3fc0c25ec273b2ac4393958
Alvin Ashcraft <73072+alvinashcraft@users.noreply.github.com>
2025-05-20 08:50:40 -0400

    Update AI app to winappsdk 1.8 exp2

--- 2025/0521_PhillyDotNet/LocalAiWinuiApp/MainWindow.xaml.cs
+++ 2025/0521_PhillyDotNet/LocalAiWinuiApp/MainWindow.xaml.cs
@@ -1,12 +1,11 @@
 using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
-using Microsoft.Windows.AI.ContentModeration;
-using Microsoft.Windows.AI.Generative;
+using Microsoft.Windows.AI;
+using Microsoft.Windows.AI.ContentSafety;
+using Microsoft.Windows.AI.Text;
 using System;
 using System.Diagnostics;
-using System.Text;
 using System.Threading.Tasks;
-using Windows.Foundation;
 
 namespace LocalAiWinuiApp
 {
@@ -23,29 +22,19 @@ namespace LocalAiWinuiApp
 
         private async void generalButton_Click(object sender, RoutedEventArgs e)
         {
-            await CallWcrAsync(LanguageModelSkill.General);
-        }
-
-        private async void rewriteButton_Click(object sender, RoutedEventArgs e)
-        {
-            await CallWcrAsync(LanguageModelSkill.Rewrite);
+            await GenerateResponseAsync();
         }
 
         private async void summarizeButton_Click(object sender, RoutedEventArgs e)
         {
-            await CallWcrAsync(LanguageModelSkill.Summarize);
+            await SummarizeTextAsync();
         }
 
-        private async void textToTableButton_Click(object sender, RoutedEventArgs e)
+        private async Task GenerateResponseAsync()
         {
-            await CallWcrAsync(LanguageModelSkill.TextToTable);
-        }
-
-        private async Task CallWcrAsync(LanguageModelSkill modelSkill)
-        {
-            if (!LanguageModel.IsAvailable())
+            if (LanguageModel.GetReadyState() == AIFeatureReadyState.NotReady)
             {
-                _ = await LanguageModel.MakeAvailableAsync();
+                _ = await LanguageModel.EnsureReadyAsync();
             }
 
             try
@@ -55,42 +44,52 @@ namespace LocalAiWinuiApp
 
                 string prompt = promptText.Text;
 
-                var modelOptions = new LanguageModelOptions
-                {
-                    Skill = modelSkill
-                };
+                var modelOptions = new LanguageModelOptions();
 
                 ContentFilterOptions filterOptions = ApplyContentFilters();
+                modelOptions.ContentFilterOptions = filterOptions;
 
-                LanguageModelResponse? result;
+                var result = await languageModel.GenerateResponseAsync(prompt, modelOptions);
 
-                if (resultWithProgress.IsChecked.Value)
-                {
-                    AsyncOperationProgressHandler<LanguageModelResponse, string>
-                    progressHandler = (asyncInfo, delta) =>
-                    {
-                        DispatcherQueue.TryEnqueue(() => { resultsMarkdown.Text = asyncInfo.GetResults().Response; });
-                    };
-
-                    var asyncOp = languageModel.GenerateResponseWithProgressAsync(prompt);
-
-                    asyncOp.Progress = progressHandler;
-
-                    result = await asyncOp;
-                }
-                else
-                {
-                    result = await languageModel.GenerateResponseAsync(modelOptions, prompt, filterOptions);
-                }
-
-                if (string.IsNullOrWhiteSpace(result.Response))
+                if (string.IsNullOrWhiteSpace(result.Text))
                 {
                     resultsMarkdown.Text = result.Status.ToString();
                 }
-                else if (!resultWithProgress.IsChecked.Value)
+                else
                 {
-                    resultsMarkdown.Text = result.Response;
+                    resultsMarkdown.Text = result.Text;
                 }
+            }
+            catch (Exception ex)
+            {
+                resultsMarkdown.Text = "An error occurred. Please try again.";
+                Debug.WriteLine(ex.Message + "; " + ex.StackTrace);
+            }
+        }
+
+        private async Task SummarizeTextAsync()
+        {
+            if (LanguageModel.GetReadyState() == AIFeatureReadyState.NotReady)
+            {
+                _ = await LanguageModel.EnsureReadyAsync();
+            }
+
+            try
+            {
+                resultsMarkdown.Text = "Thinking...";
+                using LanguageModel languageModel = await LanguageModel.CreateAsync();
+
+                string prompt = promptText.Text;
+
+                var textSummarizer = new TextSummarizer(languageModel);
+                var result = await textSummarizer.SummarizeAsync(prompt);
+
+                if (string.IsNullOrWhiteSpace(result.Text))
+                {
+                    resultsMarkdown.Text = result.Status.ToString();
+                }
+
+                resultsMarkdown.Text = result.Text;
             }
             catch (Exception ex)
             {
@@ -105,25 +104,25 @@ namespace LocalAiWinuiApp
 
             if (strictFilterCheckBox.IsChecked == true)
             {
-                filterOptions.PromptMinSeverityLevelToBlock.ViolentContentSeverity = SeverityLevel.High;
-                filterOptions.ResponseMinSeverityLevelToBlock.ViolentContentSeverity = SeverityLevel.High;
-                filterOptions.PromptMinSeverityLevelToBlock.HateContentSeverity = SeverityLevel.High;
-                filterOptions.ResponseMinSeverityLevelToBlock.HateContentSeverity = SeverityLevel.High;
-                filterOptions.PromptMinSeverityLevelToBlock.SexualContentSeverity = SeverityLevel.High;
-                filterOptions.ResponseMinSeverityLevelToBlock.SexualContentSeverity = SeverityLevel.High;
-                filterOptions.PromptMinSeverityLevelToBlock.SelfHarmContentSeverity = SeverityLevel.High;
-                filterOptions.ResponseMinSeverityLevelToBlock.SelfHarmContentSeverity = SeverityLevel.High;
+                filterOptions.PromptMaxAllowedSeverityLevel.Violent = SeverityLevel.High;
+                filterOptions.ResponseMaxAllowedSeverityLevel.Violent = SeverityLevel.High;
+                filterOptions.PromptMaxAllowedSeverityLevel.Hate = SeverityLevel.High;
+                filterOptions.ResponseMaxAllowedSeverityLevel.Hate = SeverityLevel.High;
+                filterOptions.PromptMaxAllowedSeverityLevel.Sexual = SeverityLevel.High;
+                filterOptions.ResponseMaxAllowedSeverityLevel.Sexual = SeverityLevel.High;
+                filterOptions.PromptMaxAllowedSeverityLevel.SelfHarm = SeverityLevel.High;
+                filterOptions.ResponseMaxAllowedSeverityLevel.SelfHarm = SeverityLevel.High;
             }
             else
             {
-                filterOptions.PromptMinSeverityLevelToBlock.ViolentContentSeverity = SeverityLevel.Low;
-                filterOptions.ResponseMinSeverityLevelToBlock.ViolentContentSeverity = SeverityLevel.Low;
-                filterOptions.PromptMinSeverityLevelToBlock.HateContentSeverity = SeverityLevel.Low;
-                filterOptions.ResponseMinSeverityLevelToBlock.HateContentSeverity = SeverityLevel.Low;
-                filterOptions.PromptMinSeverityLevelToBlock.SexualContentSeverity = SeverityLevel.Low;
-                filterOptions.ResponseMinSeverityLevelToBlock.SexualContentSeverity = SeverityLevel.Low;
-                filterOptions.PromptMinSeverityLevelToBlock.SelfHarmContentSeverity = SeverityLevel.Low;
-                filterOptions.ResponseMinSeverityLevelToBlock.SelfHarmContentSeverity = SeverityLevel.Low;
+                filterOptions.PromptMaxAllowedSeverityLevel.Violent = SeverityLevel.Minimum;
+                filterOptions.ResponseMaxAllowedSeverityLevel.Violent = SeverityLevel.Minimum;
+                filterOptions.PromptMaxAllowedSeverityLevel.Hate = SeverityLevel.Minimum;
+                filterOptions.ResponseMaxAllowedSeverityLevel.Hate = SeverityLevel.Minimum;
+                filterOptions.PromptMaxAllowedSeverityLevel.Sexual = SeverityLevel.Minimum;
+                filterOptions.ResponseMaxAllowedSeverityLevel.Sexual = SeverityLevel.Minimum;
+                filterOptions.PromptMaxAllowedSeverityLevel.SelfHarm = SeverityLevel.Minimum;
+                filterOptions.ResponseMaxAllowedSeverityLevel.SelfHarm = SeverityLevel.Minimum;
             }
 
             return filterOptions;
