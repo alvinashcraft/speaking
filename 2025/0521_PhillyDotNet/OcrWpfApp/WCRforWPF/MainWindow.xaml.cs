784b9e9aa1e248e641c23462e5f3721eaa2a94a8
Alvin Ashcraft <73072+alvinashcraft@users.noreply.github.com>
2025-05-21 19:05:24 -0400

    Updates

--- 2025/0521_PhillyDotNet/OcrWpfApp/WCRforWPF/MainWindow.xaml.cs
+++ 2025/0521_PhillyDotNet/OcrWpfApp/WCRforWPF/MainWindow.xaml.cs
@@ -1,23 +1,10 @@
-﻿using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
+﻿using System.Windows;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Drawing.Imaging;
 using Microsoft.Graphics.Imaging;
 using Microsoft.Windows.AI.Generative;
 using Microsoft.Windows.Vision;
-using Windows.Graphics.Imaging;
 using Windows.Storage;
-using Microsoft.Windows.AI;
 using Microsoft.Windows.AI.ContentModeration;
-using Windows.Storage.Streams;
 
 namespace WCRforWPF;
 
