0248f39e444b6b980d4284708d80eed1839714cb
Alvin Ashcraft <73072+alvinashcraft@users.noreply.github.com>
2025-09-07 17:56:19 -0400

    Add maui css sample app

---
+++ 2025/1116_VSLiveOrlando/Maui Toolkit/MauiCssApp/MauiCssApp/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiCssApp")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiCssApp.Pages")]
